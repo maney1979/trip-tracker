22b4c49c0d7dbbc48d1b2d24c6d11203ffb2e8e6
maney1979 <114640980+maney1979@users.noreply.github.com>
2025-06-01 16:15:20 -0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import Link from 'next/link'
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+const IMockToken = require('@jupyterlab/mock-token').IMockToken;
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-3xl font-bold mb-6">Welcome to Trip Tracker Beta</h1>
-      <Link href="/login" className="text-blue-500 underline mb-2">Login</Link>
-      <Link href="/new-trip" className="text-blue-500 underline mb-2">Add a Trip</Link>
-      <Link href="/trips" className="text-blue-500 underline">View Trips</Link>
-    </div>
-  )
-}
+module.exports = [
+  {
+    id: 'mock-provider',
+    provides: IMockToken,
+    autoStart: true,
+    activate: function (application) {
+      console.log('hi from provider');
+      return { value: 'hello' };
+    }
+  }
+];
